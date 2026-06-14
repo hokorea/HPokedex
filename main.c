c95155df11be5dc4a5ad18e8861070532a3b738d
hokorea <a01092012710@gmail.com>
2026-06-14 17:07:56 +0900

    Add search feature and improve menu system

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_POKEMON 151
 
@@ -14,6 +15,9 @@ typedef struct{
 	char description[512];
 } Pokemon;
 
+void clearScreen(void);
+void printTitle(void);
+void pauseScreen(void);
 void printMenu(void);
 void printPokedexList(Pokemon pokedex[], int pokemon_count);
 void printPokemonInfo(Pokemon pokemon);
@@ -24,9 +28,8 @@ int main(void){
 	int choice = 0;
 
 	while(1){
+		clearScreen();
 		printMenu();
-
-		printf("> ");
 		scanf("%d", &choice);
 
 		Pokemon bulbasaur = {
@@ -51,14 +54,22 @@ int main(void){
 		pokedex[2] = squirtle;
 
 		if(choice == 1){
+			clearScreen();
+			printTitle();
 			printPokedexList(pokedex, pokemon_count);
+			pauseScreen();
 		}
 		else if(choice == 2){
+			clearScreen();
+			printTitle();
 			int number;
 			int found = 0;
 
 			printf("도감 번호 입력 : ");
 			scanf("%d", &number);
+
+			clearScreen();
+			printTitle();
 
 			for(int i = 0; i < pokemon_count; i++){
 				if(pokedex[i].number == number){
@@ -70,6 +81,7 @@ int main(void){
 			if(found == 0){
 				printf("해당 포켓몬이 존재하지 않습니다.\n");
 			}
+			pauseScreen();
 		}
 		else if(choice == 3){
 			printf("HPokedex 종료!\n");
@@ -80,11 +92,29 @@ int main(void){
 	return 0;
 }
 
+void clearScreen(void){
+	system("clear");
+}
+
+void printTitle(void){
+	printf("+----------------------------+\n");
+	printf("|        H Pokedex           |\n");
+	printf("+----------------------------+\n");
+}
+
+void pauseScreen(void){
+	printf("\nEnter를 누르면 계속합니다...");
+	(void)getchar();
+	(void)getchar();
+}
+
 void printMenu(void){
-	printf("\n=== HPokedex ===\n");
-	printf("1. 도감 목록 보기\n");
-	printf("2. 포켓몬 상세 보기\n");
-	printf("3. 종료\n");
+	printTitle();
+	printf("| 1. 도감 목록 보기          |\n");
+	printf("| 2. 번호 검색               |\n");
+	printf("| 3. 종료                    |\n");
+	printf("+----------------------------+\n");
+	printf("> ");
 }
 
 void printPokedexList(Pokemon pokedex[], int pokemon_count){
