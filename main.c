27af8e75ee9478c4d12bff1dc72d9830ccc90cfa
hokorea <a01092012710@gmail.com>
2026-06-26 20:19:36 +0900

    Add pokemon CSV and prepare file loading

--- main.c
+++ main.c
@@ -19,6 +19,9 @@ void clearScreen(void);
 void printTitle(void);
 void pauseScreen(void);
 void printMenu(void);
+
+int loadPokedexFromCSV(Pokemon pokedex[], const char *filename);
+
 void printPokedexList(Pokemon pokedex[], int pokemon_count);
 void printPokemonInfo(Pokemon pokemon);
 
@@ -31,27 +34,6 @@ int main(void){
 		clearScreen();
 		printMenu();
 		scanf("%d", &choice);
-
-		Pokemon bulbasaur = {
-			1, "이상해씨",
-			"풀", "독", "씨앗", 0.7, 6.9, 
-			"태어났을 때부터 등에 식물의 씨앗이 있으며 조금씩 크게 자란다."
-		};
-		Pokemon charmander = {
-			4, "파이리",
-			"불꽃", "", "도마뱀", 0.6, 8.5,
-			"태어날 때부터 꼬리에 불꽃이 타오르고 있으며 불꽃이 꺼지면 생명이 위험해진다."
-		};
-	
-		Pokemon squirtle = {
-			7, "꼬부기",
-			"물", "", "꼬마거북", 0.5, 9.0,
-			"위험을 느끼면 등껍질 안에 몸을 숨기고 입에서 물을 뿜어 공격한다."
-			};
-	
-		pokedex[0] = bulbasaur;
-		pokedex[1] = charmander;
-		pokedex[2] = squirtle;
 
 		if(choice == 1){
 			clearScreen();
@@ -115,6 +97,10 @@ void printMenu(void){
 	printf("| 3. 종료                    |\n");
 	printf("+----------------------------+\n");
 	printf("> ");
+}
+
+int loadPokedexFromCSV(Pokemon pokedex[], const char *filename){
+	
 }
 
 void printPokedexList(Pokemon pokedex[], int pokemon_count){
